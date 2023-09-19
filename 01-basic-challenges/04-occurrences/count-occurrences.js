a94f355f5be0b253de9cf361260379d69a210e21
Dedios <dediosprogramming@gmail.com>
2023-09-19 00:39:03 -0400

    created occurrences dir and occurrences js file

---
+++ 01-basic-challenges/04-occurrences/count-occurrences.js
@@ -0,0 +1,22 @@
+// function called countOccurrences() that takes in a string and a character and returns the number of occurrences of that character in the string.
+
+// param - string, character
+// returns number of occurrences of the character in the string
+
+const countOccurrences = (string, character) => {
+  //loop through the string - easiest way by splitting the string
+  //if string[i] === character add it to a counter variable
+  // return counter variable.
+
+  let counter = 0;
+  let splitString = string.split('');
+
+  for (let i = 0; i < splitString.length; i++) {
+    if (character == splitString[i]) {
+      counter++;
+    }
+  }
+  return counter;
+};
+
+module.exports = countOccurrences;
