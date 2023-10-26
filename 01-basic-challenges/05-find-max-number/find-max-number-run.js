d2217be2619955bb7b4dac728d31fc812c32fa5e
Dedios <dediosprogramming@gmail.com>
2023-10-25 23:07:34 -0400

    implementing 05-find-max-number

---
+++ 01-basic-challenges/05-find-max-number/find-max-number-run.js
@@ -0,0 +1,3 @@
+const maximumNum = require('./find-max-number');
+
+console.log(maximumNum([1, 2, 3, 4, 5]));
---
+++ 01-basic-challenges/05-find-max-number/find-max-number.js
@@ -0,0 +1,6 @@
+// takes an array of numbers and returns the largest number
+
+const maxNumber = (arr) => {
+  let maxNum = Math.max(...arr);
+  return maxNum;
+};
