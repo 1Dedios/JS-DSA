befec065d86181356ca3852851337ad1d1c6fb7a
Dedios <dediosprogramming@gmail.com>
2024-01-17 22:53:06 -0500

    alt way to solve find-max-number implemented

--- 01-basic-challenges/05-find-max-number/find-max-number.js
+++ 01-basic-challenges/05-find-max-number/find-max-number.js
@@ -1,8 +1,21 @@
-// takes an array of numbers and returns the largest number
+// TAKE AN ARRAY OF NUMBERS AND RETURN THE HIGHEST VALUE
 
 const maxNumber = (arr) => {
   let maxNum = Math.max(...arr);
   return maxNum;
+};
+// ANOTHER WAY TO SOLVE:
+
+const findMaxNumber = (arr) => {
+  let maxNumber = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (maxNumber < arr[i]) {
+      maxNumber = arr[i];
+    }
+  }
+
+  return maxNumber;
 };
 
 module.exports = maxNumber;
