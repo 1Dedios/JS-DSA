6b43cc2a39620006bacc83aef372fed9671160d5
Dedios <dediosprogramming@gmail.com>
2024-05-03 22:17:50 -0400

    successfully implemented unit test of 06 title-case algo

--- 01-basic-challenges/06-title-case/title-case-run.js
+++ 01-basic-challenges/06-title-case/title-case-run.js
@@ -1,3 +1,5 @@
 const titleCase = require('./title-case');
 
 console.log(titleCase('hello my name is eddie p'));
+console.log(titleCase('i love my children'));
+console.log(titleCase('the brown fox jumped over the lazy dog'));
