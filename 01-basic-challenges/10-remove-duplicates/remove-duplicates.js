f59b6b183623ebc407afe6003e74ba572f9560f5
Dedios <dediosprogramming@gmail.com>
2024-06-13 19:08:05 -0400

    removeDuplicates algo init

---
+++ 01-basic-challenges/10-remove-duplicates/remove-duplicates.js
@@ -0,0 +1,5 @@
+// write function that takes an array and returns a new array with duplicates removed.
+
+const removeDuplicates = (array) => {
+  // take the array and create new instance of a set and turn back into array and return
+};
