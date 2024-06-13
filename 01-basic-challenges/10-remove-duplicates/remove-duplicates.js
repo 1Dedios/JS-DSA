8920ae57f6c4fad54c7a68c6209d93e5b5f92b8d
Dedios <dediosprogramming@gmail.com>
2024-06-13 19:13:31 -0400

    added comments to removeDuplicates algo

--- 01-basic-challenges/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges/10-remove-duplicates/remove-duplicates.js
@@ -2,4 +2,5 @@
 
 const removeDuplicates = (array) => {
   // take the array and create new instance of a set and turn back into array and return
+  // ANOTHER POSSIBLE SOLUTION: go through given array and if not found in the new array add it then return the new array
 };
