5346322a5336645df879f823c55b0a62a1863472
Dedios <dediosprogramming@gmail.com>
2024-06-13 19:44:19 -0400

    run file for removeDuplicates complete

---
+++ 01-basic-challenges/10-remove-duplicates/remove-duplicates-run.js
@@ -0,0 +1,8 @@
+const removeDuplicates = require('./remove-duplicates');
+
+console.log(removeDuplicates([1, 2, 3, 4, 4, 1, 2]));
+console.log(removeDuplicates([10, 22, 24, 105, 199]));
+console.log(removeDuplicates([1022, 233, 424, 10, 199, 1022]));
+console.log(removeDuplicates([104, 232, 232, 12340, 199, 104]));
+console.log(removeDuplicates([3410, 22, 24, 10, 199]));
+console.log(removeDuplicates([1032, 22, 24, 10, 199, 22]));
--- 01-basic-challenges/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges/10-remove-duplicates/remove-duplicates.js
@@ -1,6 +1,10 @@
 // write function that takes an array and returns a new array with duplicates removed.
 
 const removeDuplicates = (array) => {
-  // take the array and create new instance of a set and turn back into array and return
-  // ANOTHER POSSIBLE SOLUTION: go through given array and if not found in the new array add it then return the new array
+  // return a new array from a set
+  // ANOTHER POSSIBLE SOLUTION: go through given array and if not found in the new array add it then return the new array - using Array.includes()
+
+  return Array.from(new Set(array));
 };
+
+module.exports = removeDuplicates;
