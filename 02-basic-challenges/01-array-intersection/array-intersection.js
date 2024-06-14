d78cc16742fe2d903df01bc5d84204e5339ee92f
Dedios <dediosprogramming@gmail.com>
2024-06-13 21:13:15 -0400

    arrayIntersection algo init

---
+++ 02-basic-challenges/01-array-intersection/array-intersection.js
@@ -0,0 +1,12 @@
+// write a function called arrayIntersection that takes in two arrays and returns an array containing the intersection of the two input arrays (i.e, the common elements that appear in both array)
+
+const arrayIntersection = (array1, array2) => {
+  let intersectionArray = array1.intersection(array2);
+
+  // create a new array
+  // loop through the smallest array if the element is found in the other array as well then add to new array
+
+  // return new array
+
+  // ANOTHER WAY: is to to use the intersection() method that set has - so you would convert both arrays to sets and use the method returning the intersection. it's a matter of what you want the return value to be.
+};
