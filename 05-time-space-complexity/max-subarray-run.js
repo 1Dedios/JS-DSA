e40335716ff461d7b319ecc3a27516245a89111a
Dedios <dediosprogramming@gmail.com>
2024-06-19 16:14:00 -0400

    maxSum algo implementation

--- 05-time-space-complexity/max-subarray-run.js
+++ 05-time-space-complexity/max-subarray-run.js
@@ -1,3 +1,4 @@
 const maxSubarraySum = require('./max-subarray');
 
 console.log(maxSubarraySum([2, 3, 5, 6, 23], 2));
+console.log(maxSubarraySum([3, 3, 10, 3], 3));
--- 05-time-space-complexity/max-subarray.js
+++ 05-time-space-complexity/max-subarray.js
@@ -2,35 +2,22 @@
 // find the max sum of any subarray of length k using an O(n) solution with the sliding window technique.
 
 const maxSubarraySum = (arr, k) => {
-  // create pointers - prev, next - these are the lower bounds of each window
-  // prev < k , next <= k + prev - these are the upper bounds each window
-  // create maxSum var
-  // loop through first window and add to maxSum
-  // create subMaxSum var - loop through second window and add to subMaxSum
-  // if subMaxSum > maxSum swap
-  // increase both pointers and repeat
-
-  let prev = 0;
-  let next = 1;
+  let ptr = 0;
   let maxSum = 0;
 
-  while (next < arr.length) {
-    for (let i = prev; i < k; i++) {
-      maxSum += arr[i];
+  for (let i = 0; i < arr.length; i++) {
+    let subMax = 0;
+    ptr = i;
+    while (ptr < k + i) {
+      subMax += arr[ptr];
+      ptr++;
     }
-    for (let j = next; j <= k + prev; j++) {
-      let subarraySum = 0;
-      subarraySum += arr[j];
-
-      if (subarraySum > maxSum) {
-        maxSum = subarraySum;
-      }
+    if (subMax > maxSum) {
+      maxSum = subMax;
     }
-    prev++;
-    next++;
+    //clearing submax
+    subMax = 0;
   }
-
-  // return maxSubarray
 
   return maxSum;
 };
