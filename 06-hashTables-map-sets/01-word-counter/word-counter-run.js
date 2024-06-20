f75b98586cf6bdeaf22ab999deb854c040318708
Dedios <dediosprogramming@gmail.com>
2024-06-20 14:44:21 -0400

    refactored wordCounter runfile

--- 06-hashTables-map-sets/01-word-counter/word-counter-run.js
+++ 06-hashTables-map-sets/01-word-counter/word-counter-run.js
@@ -1,3 +1,6 @@
 const wordCounter = require('./word-counter');
 
 console.log(wordCounter('The lazy fox jumped over the other lazy fox', 'fox'));
+console.log(
+  wordCounter('The people in the house were once outside of the house', 'house')
+);
