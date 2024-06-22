211529ed28cf7b396a338d4c9d73d6f787c98521
Dedios <105307909+1Dedios@users.noreply.github.com>
2024-06-22 04:09:46 +0000

    refactoring HashTable class set() method

--- 06-hashTables-map-sets/04-custom-hash-table/custom-hash-table.js
+++ 06-hashTables-map-sets/04-custom-hash-table/custom-hash-table.js
@@ -21,15 +21,24 @@ class HashTable {
 
   // My attempts at creating the other methods
   set(key, val) {
-    this.set(key, val);
+    this.storage.push(set(key, val));
+    // this.storage.push([key, val])
+    // this.storage.push([this.set(key, val)])
+
+    // I was completely off above lol
+
+    // the index below is accessing the hash value to access the value in the array - of course to do this it requires a key
+    let index = this._hash(key, this.limit)
   }
 
   get(key) {
     return this.get(key) || null;
+    // return this.storage[key] || null;
   }
 
   remove(key) {
     this.set(key, null);
+    // this.storage.push(set(key, null))
   }
 
   has(key) {
