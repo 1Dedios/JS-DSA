32e8b3a0617a376cabfae56bfa97e26337b013f2
Dedios <dediosprogramming@gmail.com>
2024-07-12 22:25:23 -0400

    added pop & peek method to custom-stack

--- 07-stacks-queues-linkedlist/01-stack-implementation/custom-stack.js
+++ 07-stacks-queues-linkedlist/01-stack-implementation/custom-stack.js
@@ -15,6 +15,22 @@ class Stack {
     return true;
   }
 
+  pop() {
+    if (this.top < 0) {
+      return 'This stack has no members.';
+    } else {
+      return this.stack.pop();
+    }
+  }
+
+  peek() {
+    if (this.top < 0) {
+      return 'This stack has no members.';
+    } else {
+      return `The stack's next member is ${this.stack[this.top]}`;
+    }
+  }
+
   isFull() {
     return this.stack.length === this.maxSize - 1;
   }
